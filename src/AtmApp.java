fb3fbbf55bc9b57f7958de4c449401724a0fd46d
Fatih Akkaya <akkaya064@gmail.com>
2023-02-22 18:54:16 +0000

    V3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,6 +1,10 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        //Version 2
         System.out.println("Java is easy");
+        //Version 3
+        System.out.println("Learn Java Earn money");
+
     }
 }
